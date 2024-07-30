e70e99c3ac0d797790ab741b123472e65b8383d4
tanst <t@tanst.com>
2024-07-30 17:13:52 +0800

    Update update_china_ip.sh

--- sing-box/root/etc/sing-box/scripts/update_china_ip.sh
+++ sing-box/root/etc/sing-box/scripts/update_china_ip.sh
@@ -24,6 +24,11 @@ china_ipv4=$(awk '{printf (NR>1?",":"") $0}' $IPV4_TMP_FILE)
 # 读取 IPv6 CIDR 列表并拼接成一个字符串
 china_ipv6=$(awk '{printf (NR>1?",":"") $0}' $IPV6_TMP_FILE)
 
+# 清空
+nft flush set inet singbox mainland_addr_v4
+nft flush set inet singbox mainland_addr_v6
+
+#新增
 nft -f - <<EOF
 table inet singbox {
     set mainland_addr_v4 {
